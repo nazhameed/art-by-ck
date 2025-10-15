e4f587c744caec137c6f832f1e421974a093a175
curtisnlogan <curtisnlogan@gmail.com>
2025-10-15 17:32:36 +0100

    feat: add URL pattern for contactform app

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('contact/', include('contactform.urls'))
 ]
 
 if settings.DEBUG:
