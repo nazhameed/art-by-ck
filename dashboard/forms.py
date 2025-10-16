bb66281c435cc97e1b1b8d504c394c16991dcf0a
nazhameed <naz.hameed@sky.com>
2025-10-16 18:09:40 +0100

    manage pages on dashboard added and also add sections for about and exhibitions so they can also be managed on dashboard

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import GalleryImage, AvailableArt, JournalEntry
+from .models import GalleryImage, AvailableArt, JournalEntry, About, Exhibition
+from cloudinary.uploader import upload as cloudinary_upload
 
 class GalleryImageForm(forms.ModelForm):
     class Meta:
@@ -15,3 +16,35 @@ class JournalEntryForm(forms.ModelForm):
     class Meta:
         model = JournalEntry
         fields = ['title', 'image', 'content']
+
+class AboutForm(forms.ModelForm):
+    class Meta:
+        model = About
+        fields = ['text', 'image']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 6, 'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+class ExhibitionForm(forms.ModelForm):
+    class Meta:
+        model = Exhibition
+        fields = ['title', 'image', 'video', 'description', 'date', 'location']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'video': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        video_file = self.cleaned_data.get('video')
+        if video_file:
+            result = cloudinary_upload(video_file, resource_type='video')
+            instance.video = result['public_id']
+        if commit:
+            instance.save()
+        return instance
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class GalleryImage(models.Model):
@@ -29,3 +30,26 @@ class JournalEntry(models.Model):
 
     def __str__(self):
         return self.title
+
+class About(models.Model):
+    text = models.TextField()
+    image = models.ImageField(upload_to='about/', blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "About Section"
+
+class Exhibition(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='exhibition/', blank=True, null=True)
+    video = CloudinaryField('video', resource_type='video', blank=True, null=True)
+    description = models.TextField(blank=True)
+    date = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Exhibitions"
