4052a6ffadcdca34e7e78b1a0e145ac258bfe161
curtisnlogan <curtisnlogan@gmail.com>
2025-10-17 16:32:35 +0100

    feat: update URL configuration to include contact form view

--- contactform/urls.py
+++ contactform/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-# from .views import xyz
+from .views import ContactFormView, contact_form_ajax
+
+app_name = 'contactform'
 
 urlpatterns = [
-  # path('form/', .as_view(), name="contact_form")
+    path('', ContactFormView.as_view(), name='contact_form'),
 ]
